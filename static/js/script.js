14b0b006240bf98ef01e37fb691622e6ab73d045
nsrose <nsrose@berkeley.edu>
2014-07-08 20:25:13 -0700

    cover changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-
+	$("#cover_content_modal_action").click(function() {
+		$("#cover_overlay").css({
+			"opacity":"0.9",
+			"z-index":"999",
+		})
+		$("#video_modal").show();
+	})
+	$("#cover_overlay").click(function() {
+		$("#video_modal").hide();	
+		$(this).css({
+			"z-index":"0",
+			"opacity":"0.7",
+		})
+	})
+	$("#video_modal").click(function() {
+		$(this).hide();	
+		$("#cover_overlay").css({
+			"z-index":"0",
+			"opacity":"0.7",
+		})
+	})
 });
