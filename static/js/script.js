869219e86b1eba227e90da4e316d89b94773b2df
nsrose <nsrose@berkeley.edu>
2014-07-10 15:02:57 -0700

    more videos

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
 var video_ids = {
 	"play_video_0":"//player.vimeo.com/video/76762545",
+	"play_video_a":"//player.vimeo.com/video/98451634",
 	"play_video_1":"//player.vimeo.com/video/97086004",
+	"play_video_2":"//player.vimeo.com/video/88093599",
+	"play_video_3":"//player.vimeo.com/video/95032876",
+	"play_video_4":"//www.youtube.com/embed/-c2U2idnWqU",
 
 }
 
@@ -23,6 +27,14 @@ $(document).ready(function(){
 		})
 		$("#video_modal").show();
 		video_chooser("play_video_0");
+	})
+	$("#play_video_a").click(function() {
+		$("#main_overlay").css({
+			"opacity":"0.9",
+			"z-index":"997",
+		})
+		$("#video_modal").show();
+		video_chooser("play_video_a");
 	})
 
 	$("#main_overlay").click(function() {
@@ -55,13 +67,20 @@ $(document).ready(function(){
 
 	// Hover and animation:
 	$(".section_video").mouseenter(function() {
-		$(".section_video_overlay").css({
+		var section_id = $(this).parent().find("div").attr("id")
+		$("#" + section_id).css({
+			"opacity":"0.9",
 			"top":"0",
 		})
 	})
 	$(".section_video_overlay").mouseleave(function() {
+		
 		$(this).css({
 			"top":"-150",
+			"opacity":"0",
 		})
+		
+		
+		
 	})
 });
