0b92ca477d1ac291fee187b2acdf685bcbe3db0f
nsrose <nsrose@berkeley.edu>
2014-07-09 18:45:20 -0700

    videos section

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
+	
+	// Video modal control:
 	$("#cover_content_modal_action").click(function() {
-		$("#cover_overlay").css({
+		$("#main_overlay").css({
 			"opacity":"0.9",
-			"z-index":"999",
+			"z-index":"997",
 		})
 		$("#video_modal").show();
 	})
-	$("#cover_overlay").click(function() {
+	$("#main_overlay").click(function() {
 		$("#video_modal").hide();	
 		$(this).css({
 			"z-index":"0",
@@ -15,9 +17,33 @@ $(document).ready(function(){
 	})
 	$("#video_modal").click(function() {
 		$(this).hide();	
-		$("#cover_overlay").css({
+		$("#main_overlay").css({
 			"z-index":"0",
 			"opacity":"0.6",
+		})
+	})
+	$(".section_video_overlay_play").click(function() {
+		var video_id = $(this).attr('id');
+
+
+		$("#main_overlay").css({
+			"opacity":"0.9",
+			"z-index":"999",
+		})
+		$("#video_modal").show();
+	})
+
+
+
+	// Hover and animation:
+	$(".section_video").mouseenter(function() {
+		$(".section_video_overlay").css({
+			"top":"0",
+		})
+	})
+	$(".section_video_overlay").mouseleave(function() {
+		$(this).css({
+			"top":"-150",
 		})
 	})
 });
